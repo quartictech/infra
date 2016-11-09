ebab305fc11108f9e091c3d23e02132bfd72e964
Alex Sparrow <alex@quartic.io>
2016-11-09 17:55:43 +0000

    Add import job and fix stuff

--- kubernetes/dilectic_restart.sh
+++ kubernetes/dilectic_restart.sh
@@ -1,5 +1,5 @@
 kubectl --namespace dilectic apply -f dilectic
-kubectl --namespace dilectic delete -f dilectic/dilectic-deployment.yml
-kubectl --namespace dilectic create -f dilectic/dilectic-deployment.yml
+kubectl --namespace dilectic delete -f dilectic/dilectic.yml
+kubectl --namespace dilectic create -f dilectic/dilectic.yml
 kubectl --namespace dilectic get pods
 
