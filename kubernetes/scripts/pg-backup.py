e7e3178afcd9cb0d595b9d3b9a3e3dd0efcf3e5b
Oliver Charlesworth <oliver@quartic.io>
2017-08-14 17:51:29 +0100

    WIP

--- kubernetes/scripts/pg-backup.py
+++ kubernetes/scripts/pg-backup.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 # pylint: disable=C0111
 
+# TODO - image requires "apt-get install postgresql-client" (what about version mismatch?)
+
 import atexit
 import logging
+import subprocess
 import sys
 from os import path
 from pprint import pprint
@@ -11,9 +14,35 @@ from kubernetes import client, config
 from kubernetes.client.rest import ApiException
 import yaml
 
-# TODO - this should be randomised and then logged
+PG_HOST = "dummy-postgres"
+PG_USER = "postgres"
+PG_DATABASE = "postgres"
+
+DUMP_FILE = "/db.sql"
+
 SOURCE_SERVICE = "postgres"
-TARGET_NAMESPACE = "backups"
+TARGET_NAMESPACE = "backups"    # TODO - this should be randomised and then logged
+
+
+# Modified http://blog.endpoint.com/2015/01/getting-realtime-output-using-python.html for Python3 and stdin
+def run_command(command, stdin=""):
+    process = subprocess.Popen(command, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    process.stdin.write(str.encode(stdin))
+    process.stdin.close()
+    while True:
+        output = process.stdout.readline().decode()
+        if output == "" and process.poll() is not None:
+            break
+        if output:
+            logging.info(output, end="")
+    rc = process.poll()
+    return rc
+
+
+def dump_postgres():
+    rc = run_command(["pg_dump", "-h", PG_HOST, "-U", PG_USER, "-f", PG_DATABASE, DUMP_FILE])
+    if (rc != 0):
+        logging.error("pg_dump failed with exit status {}", rc)
 
 
 def cleanup():
@@ -47,40 +76,47 @@ def create_namespace_if_non_existent():
         sys.exit(1)
 
 
-def create_service():
-    api = client.CoreV1Api()
+# def create_service():
+#     api = client.CoreV1Api()
 
-    logging.info("Creating service")
-    with open(path.join(path.dirname(__file__), "service.yml")) as f:
-        body = yaml.load(f)
-        try:
-            api.create_namespaced_service(NAMESPACE, body)
-        except ApiException:
-            logging.exception("Couldn't create service")
-            sys.exit(1)
+#     logging.info("Creating service")
+#     with open(path.join(path.dirname(__file__), "service.yml")) as f:
+#         body = yaml.load(f)
+#         try:
+#             api.create_namespaced_service(TARGET_NAMESPACE, body)
+#         except ApiException:
+#             logging.exception("Couldn't create service")
+#             sys.exit(1)
 
 
-def create_stateful_set():
-    api = client.AppsV1beta1Api()
+# def create_stateful_set():
+#     api = client.AppsV1beta1Api()
 
-    logging.info("Creating stateful set")
-    with open(path.join(path.dirname(__file__), "stateful-set.yml")) as f:
-        body = yaml.load(f)
-        try:
-            api.create_namespaced_stateful_set(NAMESPACE, body)
-        except ApiException:
-            logging.exception("Couldn't create stateful set")
-            sys.exit(1)
+#     logging.info("Creating stateful set")
+#     with open(path.join(path.dirname(__file__), "stateful-set.yml")) as f:
+#         body = yaml.load(f)
+#         try:
+#             api.create_namespaced_stateful_set(TARGET_NAMESPACE, body)
+#         except ApiException:
+#             logging.exception("Couldn't create stateful set")
+#             sys.exit(1)
+
+
+def load_api_config():
+    try:
+        config.load_incluster_config()
+    except:
+        config.load_kube_config()
 
 
 def run():
     logging.basicConfig(level=logging.INFO, format='%(levelname)s [%(asctime)s] %(name)s: %(message)s')
 
-    config.load_kube_config()
+    load_api_config()
 
     # TODO - S3 rotation
 
-    # TODO - pg_dump
+    dump_postgres()
 
     # TODO - upload to S3
 
