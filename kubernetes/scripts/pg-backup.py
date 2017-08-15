84da1b5a33026420b3f688b170c895ba9f7a520d
Oliver Charlesworth <oliver@quartic.io>
2017-08-15 12:41:02 +0100

    WIP

--- kubernetes/scripts/pg-backup.py
+++ kubernetes/scripts/pg-backup.py
@@ -14,14 +14,16 @@ from kubernetes import client, config
 from kubernetes.client.rest import ApiException
 import yaml
 
-PG_HOST = "dummy-postgres"
-PG_USER = "postgres"
-PG_DATABASE = "postgres"
+SOURCE_PG_HOST = "dummy-postgres"
+SOURCE_PG_USER = "postgres"
+SOURCE_PG_DATABASE = "postgres"
 
 DUMP_FILE = "/db.sql"
 
-SOURCE_SERVICE = "postgres"
-TARGET_NAMESPACE = "backups"    # TODO - this should be randomised and then logged
+TEMP_NAMESPACE = "backups"    # TODO - this should be randomised and then logged
+TEMP_PG_HOST = "postgres"
+TEMP_PG_USER = "postgres"
+TEMP_PG_DATABASE = "postgres"
 
 
 # Modified http://blog.endpoint.com/2015/01/getting-realtime-output-using-python.html for Python3 and stdin
@@ -40,9 +42,10 @@ def run_command(command, stdin=""):
 
 
 def dump_postgres():
-    rc = run_command(["pg_dump", "-h", PG_HOST, "-U", PG_USER, "-f", PG_DATABASE, DUMP_FILE])
+    rc = run_command(["pg_dump", "-h", SOURCE_PG_HOST, "-U", SOURCE_PG_USER, "-f", DUMP_FILE, SOURCE_PG_DATABASE])
     if (rc != 0):
-        logging.error("pg_dump failed with exit status {}", rc)
+        logging.error("pg_dump failed with exit status %d", rc)
+        sys.exit(1)
 
 
 def cleanup():
@@ -51,7 +54,7 @@ def cleanup():
     logging.info("Deleting namespace")
     try:
         body = client.V1DeleteOptions()
-        api.delete_namespace(TARGET_NAMESPACE, body)
+        api.delete_namespace(TEMP_NAMESPACE, body)
     except ApiException:
         logging.exception("Couldn't delete namespace")
 
@@ -60,46 +63,46 @@ def create_namespace_if_non_existent():
     api = client.CoreV1Api()
 
     try:
-        api.read_namespace(TARGET_NAMESPACE)
+        api.read_namespace(TEMP_NAMESPACE)
         logging.info("Namespace already exists")
         return
     except ApiException:
         pass
 
     logging.info("Creating namespace")
+    body = client.V1Namespace()
+    body.metadata = client.V1ObjectMeta(name=TEMP_NAMESPACE)
     try:
-        body = client.V1Namespace()
-        body.metadata = client.V1ObjectMeta(name=TARGET_NAMESPACE)
         api.create_namespace(body)
     except ApiException:
         logging.exception("Couldn't create namespace")
         sys.exit(1)
 
 
-# def create_service():
-#     api = client.CoreV1Api()
+def do_something_with_file(filename, action, description):
+    logging.info(description)
+    api = client.CoreV1Api()
+    with open(path.join(path.dirname(__file__), filename)) as f:
+        body = yaml.load(f)
+        try:
+            action(api, body)
+        except ApiException:
+            logging.exception("%s failed", description)
+            sys.exit(1)
 
-#     logging.info("Creating service")
-#     with open(path.join(path.dirname(__file__), "service.yml")) as f:
-#         body = yaml.load(f)
-#         try:
-#             api.create_namespaced_service(TARGET_NAMESPACE, body)
-#         except ApiException:
-#             logging.exception("Couldn't create service")
-#             sys.exit(1)
+
+def create_postgres_service():
+    do_something_with_file(
+        "pg_service.yml",
+        lambda api, body: api.create_namespaced_service(TEMP_NAMESPACE, body),
+        "Creating Postgres service")
 
 
-# def create_stateful_set():
-#     api = client.AppsV1beta1Api()
-
-#     logging.info("Creating stateful set")
-#     with open(path.join(path.dirname(__file__), "stateful-set.yml")) as f:
-#         body = yaml.load(f)
-#         try:
-#             api.create_namespaced_stateful_set(TARGET_NAMESPACE, body)
-#         except ApiException:
-#             logging.exception("Couldn't create stateful set")
-#             sys.exit(1)
+def create_postgres_deployment():
+    do_something_with_file(
+        "pg_deployment.yml",
+        lambda api, body: api.create_namespaced_deployment(TEMP_NAMESPACE, body),
+        "Creating Postgres deployment")
 
 
 def load_api_config():
@@ -109,33 +112,34 @@ def load_api_config():
         config.load_kube_config()
 
 
-def run():
-    logging.basicConfig(level=logging.INFO, format='%(levelname)s [%(asctime)s] %(name)s: %(message)s')
-
-    load_api_config()
-
+def backup():
     # TODO - S3 rotation
 
     dump_postgres()
 
     # TODO - upload to S3
 
+
+def test_restore():
     # Create namespace to perform test restoration in
     create_namespace_if_non_existent()
     atexit.register(cleanup)
 
-    # TODO - create PG service
+    create_postgres_service()
+    create_postgres_deployment()
+
+    # TODO - wait until ready
+
+    # TODO - download from S3
 
     # TODO - pg_restore into temp PG
 
     # TODO - do some dummy operation against temp PG
 
 
-
-    create_service()
-    create_stateful_set()
-
-
 if __name__ == "__main__":
-    run()
+    logging.basicConfig(level=logging.INFO, format='%(levelname)s [%(asctime)s] %(name)s: %(message)s')
+    load_api_config()
+    backup()
+    test_restore()
 
