a6a282f2661067ec9b8fd1339af0ea28e634c82d
Oliver Charlesworth <oliver@quartic.io>
2017-08-15 18:22:18 +0100

    Sort out perma-postgres

--- kubernetes/backups/scripts/pg-backup.sh
+++ kubernetes/backups/scripts/pg-backup.sh
@@ -17,7 +17,7 @@ GCS_URL="gs://${GCS_BUCKET}/postgres/postgres.$(date -u +"%Y-%m-%dT%H:%M:%SZ").$
 # If we don't use --data-only, then we end up trying to restore e.g. "postgres" role, which causes errors on restore
 # We assume that users, etc. are created programmatically
 echo "Running pg_dumpall ..."
-pg_dumpall -h ${POSTGRES_HOST} -U ${POSTGRES_USER} --data-only | gzip > ${DUMP_FILE}
+pg_dumpall -h ${SOURCE_POSTGRES_HOST} -U ${SOURCE_POSTGRES_USER} --clean --data-only | gzip > ${DUMP_FILE}
 
 echo "Uploading to ${GCS_URL} ..."
 gsutil cp ${DUMP_FILE} ${GCS_URL}
@@ -29,12 +29,7 @@ gsutil cp ${DUMP_FILE} ${GCS_URL}
 echo "Downloading from ${GCS_URL} ..."
 gsutil cp ${GCS_URL} ${RESTORE_FILE}
 
-echo "Waiting for local Postgres to become ready ..."
-until pg_isready -h localhost; do
-    sleep 1
-done
-
 echo "Restoring to temp Postgres instance ..."
-gunzip < ${RESTORE_FILE} | psql -h localhost -U postgres -d postgres -q
+gunzip < ${RESTORE_FILE} | psql -h ${TEMP_POSTGRES_HOST} -U ${TEMP_POSTGRES_USER} -d postgres -q
 
 echo "Complete!"
