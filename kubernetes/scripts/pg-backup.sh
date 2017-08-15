10d4381a45478bacc9772942f3c3562eaecf1882
Oliver Charlesworth <oliver@quartic.io>
2017-08-15 16:03:30 +0100

    Get E2E script working

--- kubernetes/scripts/pg-backup.sh
+++ kubernetes/scripts/pg-backup.sh
@@ -1,8 +1,6 @@
 #! /bin/bash
-set -eu
+set -euo pipefail
 
-# TODO - image requires "apt-get install postgresql-client" (what about version mismatch?)
-# TODO - also requires gsutil - https://cloud.google.com/storage/docs/gsutil_install#deb
 # TODO - parameterise all of this
 # TODO - secure credentials?
 
@@ -12,37 +10,30 @@ SOURCE_PG_HOST="dummy-postgres"
 SOURCE_PG_USER="postgres"
 SOURCE_PG_DATABASE="postgres"
 
-TEMP_NAMESPACE="backups"    # TODO - this should be randomised and then logged
-TEMP_PG_HOST="postgres"
-TEMP_PG_USER="postgres"
-TEMP_PG_DATABASE="postgres"
-
 DUMP_FILE="/db.sql.gz"
-RESTORE_FILE="/restore.sql"
+RESTORE_FILE="/restore.sql.gz"
 
-GCS_URL="gs://${GCS_BUCKET}/postgres/db.$(date -u +"%Y-%m-%dT%H:%M:%SZ").sql"
+GCS_URL="gs://${GCS_BUCKET}/postgres/db.$(date -u +"%Y-%m-%dT%H:%M:%SZ").sql.gz"
 
-function dump_to_local() {
-    echo "Running pg_dump ..."
-    pg_dump -h ${SOURCE_PG_HOST} -U ${SOURCE_PG_USER} ${SOURCE_PG_DATABASE} | gzip | ${DUMP_FILE}
-}
-
-function upload_to_gcs() { 
-    echo "Uploading to ${GCS_URL} ..."
-    gsutil -m rsync ${DUMP_FILE} ${GCS_URL}
-}
-
-function download_from_gcs() { 
-    echo "Downloading from ${GCS_URL} ..."
-    gsutil -m rsync ${GCS_URL} ${RESTORE_FILE}
-}
-
+#----------------------------------------#
 # Backup
-dump_to_local
-upload_to_gcs
+#----------------------------------------#
 
+echo "Running pg_dump ..."
+pg_dump -h ${SOURCE_PG_HOST} -U ${SOURCE_PG_USER} ${SOURCE_PG_DATABASE} | gzip > ${DUMP_FILE}
+
+echo "Uploading to ${GCS_URL} ..."
+gsutil cp ${DUMP_FILE} ${GCS_URL}
+
+#----------------------------------------#
 # Test restore
-download_from_gcs
-# TODO - PG restore
+#----------------------------------------#
+
+echo "Downloading from ${GCS_URL} ..."
+gsutil cp ${GCS_URL} ${RESTORE_FILE}
+
+echo "Restoring to temp Postgres instance ..."
+gunzip < ${RESTORE_FILE} | psql -h localhost -U postgres -d postgres -q
+
 # TODO - test query or something
 
