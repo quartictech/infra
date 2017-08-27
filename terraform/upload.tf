a11dfc93ac3311041ab7032c4c080d3e0f7b4997
Oliver Charlesworth <oliver@quartic.io>
2017-08-27 16:56:31 +0100

    Switch things over to import www static IP

--- terraform/upload.tf
+++ terraform/upload.tf
@@ -1,3 +1,5 @@
 resource "google_compute_address" "upload" {
     name            = "upload"
 }
+
+// TODO - add instance
--- terraform/www.tf
+++ terraform/www.tf
@@ -1,29 +1,29 @@
 resource "google_compute_address" "www" {
-    name            = "www-dummy"   # TODO - replace once happy
+    name            = "www"
 }
 
-resource "google_compute_instance" "www" {
-    name            = "www-dummy"   # TODO - replace once happy
-    machine_type    = "g1-small"
-    zone            = "${var.zone}"
+# resource "google_compute_instance" "www" {
+#     name            = "www-dummy"   # TODO - replace once happy
+#     machine_type    = "g1-small"
+#     zone            = "${var.zone}"
 
-    tags            = ["http-server", "https-server"]
+#     tags            = ["http-server", "https-server"]
 
-    boot_disk {
-        initialize_params {
-            image   = "debian-cloud/debian-9"
-        }
-    }
+#     boot_disk {
+#         initialize_params {
+#             image   = "debian-cloud/debian-9"
+#         }
+#     }
 
-    network_interface {
-        network     = "default"
+#     network_interface {
+#         network     = "default"
 
-        access_config {
-            nat_ip  = "${google_compute_address.www.address}"
-        }
-    }
+#         access_config {
+#             nat_ip  = "${google_compute_address.www.address}"
+#         }
+#     }
 
-    service_account {
-        scopes = ["compute-rw"]
-    }
-}
+#     service_account {
+#         scopes = ["compute-rw"]
+#     }
+# }
