5505c0055b60ee63431a3e3e710f967a4a4b2447
Oliver Charlesworth <oliver@quartic.io>
2017-09-21 14:12:58 +0100

    Add staging worker pool

--- terraform/staging/modules/cluster/cluster.tf
+++ terraform/staging/modules/cluster/cluster.tf
@@ -16,6 +16,24 @@ resource "google_compute_address" "cluster" {
 }
 
 
+# See https://www.terraform.io/docs/providers/google/r/container_node_pool.html#oauth_scopes
+variable "basic_gke_oauth_scopes" {
+    default = [
+        "https://www.googleapis.com/auth/compute",
+        "https://www.googleapis.com/auth/devstorage.read_only",
+        "https://www.googleapis.com/auth/logging.write",
+        "https://www.googleapis.com/auth/monitoring",
+    ]
+}
+
+
+variable "extra_gke_oauth_scopes" {
+    default = [
+        "https://www.googleapis.com/auth/datastore",        # TODO - get rid of datastore
+    ]
+}
+
+
 resource "google_container_cluster" "cluster" {
     project             = "${var.project_id}"
     zone                = "${var.zones[0]}"
@@ -27,16 +45,26 @@ resource "google_container_cluster" "cluster" {
     node_config {
         machine_type    = "n1-standard-4"
 
-        oauth_scopes    = [
-            "https://www.googleapis.com/auth/compute",
-            "https://www.googleapis.com/auth/devstorage.read_only",
-            "https://www.googleapis.com/auth/logging.write",
-            "https://www.googleapis.com/auth/monitoring",
-            # TODO - can remove datastore eventually
-            "https://www.googleapis.com/auth/datastore",
-        ]
+        oauth_scopes    = "${concat(var.basic_gke_oauth_scopes, var.extra_gke_oauth_scopes)}"
     }
 }
+
+
+resource "google_container_node_pool" "worker" {
+    project             = "${var.project_id}"
+    zone                = "${var.zones[0]}"
+    name                = "worker"
+    cluster             = "${google_container_cluster.cluster.name}"
+
+    initial_node_count  = 1
+
+    node_config {
+        machine_type    = "n1-standard-2"
+
+        oauth_scopes    = "${var.basic_gke_oauth_scopes}"
+    }
+}
+
 
 output "address" {
     value = "${google_compute_address.cluster.address}"
