396185ffc3ab0c432303a6906e79e21b09753274
Oliver Charlesworth <oliver@quartic.io>
2017-10-26 20:31:24 +0100

    Make node-pool preemption configurable

--- terraform/env/_modules/env/env.tf
+++ terraform/env/_modules/env/env.tf
@@ -10,8 +10,10 @@ variable "dns_ttl"                      {}
 variable "cluster_name"                 {}
 variable "cluster_core_node_type"       {}
 variable "cluster_core_node_count"      {}
+variable "cluster_core_preemptible"     {}
 variable "cluster_worker_node_type"     {}
 variable "cluster_worker_node_count"    {}
+variable "cluster_worker_preemptible"   {}
 variable "cluster_full_access"          {}
 
 
@@ -71,10 +73,14 @@ module "cluster" {
     zones                       = "${data.google_compute_zones.available.names}"
     name                        = "${var.cluster_name}"
     service_account_email       = "${module.iam.cluster_service_account_email}"
+
     core_node_type              = "${var.cluster_core_node_type}"
     core_node_count             = "${var.cluster_core_node_count}"
+    core_preemptible            = "${var.cluster_core_preemptible}"
+
     worker_node_type            = "${var.cluster_worker_node_type}"
     worker_node_count           = "${var.cluster_worker_node_count}"
+    worker_preemptible          = "${var.cluster_worker_preemptible}"
 }
 
 module "dns" {
--- terraform/env/prod.tfvars
+++ terraform/env/prod.tfvars
@@ -7,7 +7,9 @@ dns_ttl                     = 3600
 cluster_name                = "prod"
 cluster_core_node_type      = "n1-standard-2"
 cluster_core_node_count     = 2
+cluster_core_preemptible    = false
 cluster_worker_node_type    = "n1-standard-2"
 cluster_worker_node_count   = 1
+cluster_worker_preemptible  = false
 
 container_developer_group   = "core@quartic.io"             # TODO - this can't be true long-term
