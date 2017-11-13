79aa52927eb8d03789bc4facb4e473dfced33df0
Oliver Charlesworth <oliver@quartic.io>
2017-11-13 11:12:15 +0000

    Use a CNAME record to point www.quartic.io at GitHub pages

--- terraform/env/_modules/dns/dns.tf
+++ terraform/env/_modules/dns/dns.tf
@@ -3,7 +3,7 @@ variable "global_zone_name"     {}
 variable "domain_name"          {}
 variable "ttl"                  {}
 variable "cluster_address"      {}
-variable "cluster_subdomains"   { default = ["*.", "", "www."] } # TODO - shouldn't need "www" here, but something weird happening with GCloud DNS
+variable "cluster_subdomains"   { default = ["*.", ""] }
 
 
 # NOTE: We set these in global DNS
@@ -15,4 +15,14 @@ resource "google_dns_record_set" "cluster" {
     ttl             = "${var.ttl}"
     type            = "A"
     rrdatas         = ["${var.cluster_address}"]
+}
+
+# NOTE: this is a hack so that we're using GitHub pages to serve up the website
+resource "google_dns_record_set" "www" {
+    project         = "${var.global_project_id}"
+    name            = "www.${var.domain_name}"
+    managed_zone    = "${var.global_zone_name}"
+    ttl             = "${var.ttl}"
+    type            = "CNAME"
+    rrdatas         = ["quartictech.github.io"]
 }
